25b4a62886479644cfe3d6b2891e2c8f9f9e02a3
Usheninte <jrandangana@gmail.com>
2018-09-20 20:51:07 +0100

    build

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 React web app for Realworld Beautician business
 #### [Go to React app](https://realworld-beautician.netlify.com/)
 
+Realworld Beautician is a business that offers beauty enhancement servives. It is located in Ibadan, Nigeria."
+
 - - -
 
 # [Built by Usheninte](https://twitter.com/Usheninte)
--- src/components/views/gallery.js
+++ src/components/views/gallery.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Image } from 'react-bootstrap';
 import { Grid } from 'react-bootstrap';
 import { Row } from 'react-bootstrap';
 import { Col } from 'react-bootstrap';
@@ -17,28 +18,36 @@ export default class Gallery extends Component {
         <hr />
         <Grid>
           <Row>
-            <Col xs={12} md={4}>
+            <Col xs={12} md={4} className="pic">
+              <Image alt="Clients" title="Clients" src={'https://res.cloudinary.com/poetrique/image/upload/v1537472407/clients/realworld_beautician/realworldrb____utm_source_ig_share_sheet_igshid_d2gfygmsisx5___.jpg'} thumbnail responsive />
             </Col>
-            <Col xs={12} md={4}>
+            <Col xs={12} md={4} className="pic">
+              <Image alt="Clients" title="Clients" src={'https://res.cloudinary.com/poetrique/image/upload/v1537472407/clients/realworld_beautician/realworldrb____utm_source_ig_share_sheet_igshid_zkqgwn3unevh___.jpg'} thumbnail responsive />
             </Col>
-            <Col xs={12} md={4}>
-            </Col>
-          </Row>
-          <Row>
-            <Col xs={12} md={4}>
-            </Col>
-            <Col xs={12} md={4}>
-            </Col>
-            <Col xs={12} md={4}>
+            <Col xs={12} md={4} className="pic">
+              <Image alt="Clients" title="Clients" src={'https://res.cloudinary.com/poetrique/image/upload/v1537472397/clients/realworld_beautician/realworldrb____utm_source_ig_share_sheet_igshid_e4eqk0ef77x___.jpg'} thumbnail responsive />
             </Col>
           </Row>
           <Row>
-            <Col xs={12} md={4}>
+            <Col xs={12} md={4} className="pic">
+              <Image alt="Clients" title="Clients" src={'https://res.cloudinary.com/poetrique/image/upload/v1537472397/clients/realworld_beautician/realworldrb____utm_source_ig_share_sheet_igshid_6kylnfwcbvjj___.jpg'} thumbnail responsive />
             </Col>
-            <Col xs={12} md={4}>
+            <Col xs={12} md={4} className="pic">
+              <Image alt="Clients" title="Clients" src={'https://res.cloudinary.com/poetrique/image/upload/v1537472396/clients/realworld_beautician/realworldrb____utm_source_ig_share_sheet_igshid_4mfgle5spgkm___.jpg'} thumbnail responsive />
             </Col>
-            <Col xs={12} md={4}>
+            <Col xs={12} md={4} className="pic">
+              <Image alt="Clients" title="Clients" src={'https://res.cloudinary.com/poetrique/image/upload/v1537472396/clients/realworld_beautician/realworldrb____utm_source_ig_share_sheet_igshid_1tlm60axfsmo___.jpg'} thumbnail responsive />
             </Col>
+          </Row>
+          <Row className="twin-inverse">
+            <Col xsHidden md={2}></Col>
+            <Col xs={12} md={4} className="pic">
+              <Image alt="Clients" title="Clients" src={'https://res.cloudinary.com/poetrique/image/upload/v1537472395/clients/realworld_beautician/realworldrb____utm_source_ig_share_sheet_igshid_2stwsnw2n2wi___.jpg'} thumbnail responsive />
+            </Col>
+            <Col xs={12} md={4} className="pic">
+              <Image alt="Clients" title="Clients" src={'https://res.cloudinary.com/poetrique/image/upload/v1537472395/clients/realworld_beautician/realworldrb____utm_source_ig_share_sheet_igshid_1emxvrkmququk___.jpg'} thumbnail responsive />
+            </Col>
+            <Col xsHidden md={2}></Col>
           </Row>
         </Grid>
       </div>
